44ffe24b66124ccd84a4cd7f6c3cfe8032216210
damiancrue <damian.aguilera@outlook.com>
2023-12-05 00:58:12 -0300

    actualizacion

--- README.md
+++ README.md
@@ -1,36 +1,44 @@
 # tpo-codo-a-codo
-1. Título y Descripción del Proyecto:
-  - WEB < INDUMENTARIA >
-  - Portal en el que se puedan exhibir los productos de indumentaria deportiva, realizar reservas, solicitar envíos ó realizar pagos de los mismos.
-3. Público Objetivo:
-  - Personas de 18 a 50 años que tengan conocimientos de navegación web y que procuren adquirir alguno de los productos ofrecidos.
-4. Estructura del Sitio:
-  - Esquema compueto por 4 secciones principales:
-    - HOME: que contiene el portal principal de ofertas de productos y contiene las subpáginas.
-      - CATALOGO: muestra de los productos exhibidos.
-      - CARRITO: contiene los productos marcados para comprar.  
-      - CONTACTO: contiene formulario de contacto.
-      - FAQ: preguntas frequentes.
-      - NOSOTROS: breve descripcion de la empresa.
-4. Diseño y Estilo:
-  - Se pretende tener un estilo y diseño modernos y llamativos con paleta de colores suave para no producir cansancio a la vista. Diseño intuitivo que sea sencillo de usar en todos los dispositivos y formatos.
-5. Contenido y Funcionalidades:
-  - Texto.
-  - Imágenes.
-  - Videos.
-  - Animaciones.
-  - Canal de Pago.
-  - Formularios.
-  - Barra de búsqueda.
-6. Responsabilidades del Equipo:
-  - A designar.
-7. Cronograma Tentativo:
-  - A proyectar.
-8. Recursos Externos:
-  - A Planificar.
-9. Desarrollos Futuros y Mejoras Potenciales (Opcional)
-  - En estudio.
-10. Contacto:
-  - A diagramar.
-11. Fecha de Entrega:
-  - Mes de Octubre 2023.
+
+Título del Proyecto: Tienda Online de Indumentaria urbana
+
+Descripción del Proyecto: 
+
+El proyecto consiste en la creación de una tienda web dedicada a la exhibición y venta de productos de indumentaria urbana. La tienda permitirá a los usuarios explorar, seleccionar y comprar productos, solicitar envíos y efectuar pagos de manera conveniente. El diseño del sitio se orientará hacia un enfoque moderno y atractivo, con una paleta de colores suaves para garantizar la comodidad visual de los usuarios. Además, se incorporarán funcionalidades como formularios de contacto, preguntas frecuentes y un diseño intuitivo que se adapte a distintos dispositivos y formatos.
+
+Público Objetivo: Nuestro público objetivo se compone de personas de 18 a 50 años con interés en adquirir productos de indumentaria Urbana. Nos esforzaremos por ofrecer una experiencia de usuario amigable y atractiva para satisfacer sus necesidades y preferencias.
+Estructura del Sitio: El sitio web constará de las siguientes secciones principales:
+
+1.	HOME: La página principal que mostrará las ofertas de productos y proporcionará acceso a las subpáginas.
+2.	CATÁLOGO: Una sección que exhibirá los productos disponibles para su compra.
+3.	CARRITO: Un espacio donde los usuarios podrán agregar los productos que desean comprar.
+4.	CONTACTO: Una sección que contendrá un formulario de contacto para consultas y soporte.
+5.	FAQ: Preguntas frecuentes para ayudar a los usuarios a resolver dudas comunes.
+6.	NOSOTROS: Una breve descripción de la empresa y su misión.
+
+
+Diseño y Estilo: El diseño se caracterizará por ser moderno y atractivo, con una paleta de colores suaves para evitar fatigar la vista de los usuarios. El sitio será intuitivo y fácil de usar en todos los dispositivos y formatos, lo que garantizará una experiencia de usuario satisfactoria.
+
+Contenido y Funcionalidades: El sitio incorporará una variedad de elementos y funcionalidades, entre ellos:
+
+1.	Texto descriptivo de productos y la empresa.
+2.	Imágenes de alta calidad que muestren los productos.
+3.	Videos y animaciones para presentar los productos de manera efectiva.
+4.	Canal de pago seguro para facilitar las transacciones.
+5.	Formularios de contacto para consultas y atención al cliente.
+6.	Barra de búsqueda para ayudar a los usuarios a encontrar productos específicos.
+
+Responsabilidades del Equipo: Las responsabilidades del equipo se asignarán durante la planificación del proyecto, y se definirán de acuerdo con las habilidades y experiencia de cada miembro.
+
+Cronograma Tentativo: El cronograma detallado del proyecto se establecerá durante la fase de planificación y será comunicado a todos los miembros del equipo.
+
+Recursos Externos: Se planificarán y asignarán los recursos externos necesarios para llevar a cabo el proyecto de manera eficiente, como proveedores de indumentaria, servicios de envío y pago, entre otros.
+
+Desarrollos Futuros y Mejoras Potenciales : Se considerarán posibles desarrollos futuros y mejoras adicionales en el sitio web, pero estos se definirán y planificarán en una etapa posterior.
+
+Contacto: La sección de contacto se implementará en el sitio web, y se proporcionará información de contacto para que los usuarios puedan comunicarse con la empresa.
+
+Fecha de Entrega: 
+El proyecto tiene como fecha de entrega 28/10/2023. 
+Se trabajará para cumplir con este plazo y garantizar la calidad y funcionalidad del sitio web.
+
---
+++ js/script.js
@@ -0,0 +1,306 @@
+if (document.querySelector(".gallery__item")) {
+
+	const container = document.querySelector(".container");
+
+	const galleryItem = document.querySelectorAll(".gallery__item");
+
+	const cleaner = () => {
+
+		galleryItem.forEach((gallery) => {
+
+			gallery.classList.remove("--active");
+
+		});
+
+	};
+
+	galleryItem.forEach((gallery, i) => {
+
+		gallery.addEventListener("click", () => {
+
+			cleaner();
+
+			gallery.classList.add("--active");
+
+			if (i === 0) {
+
+				container.style.backgroundColor = "#0033004d";
+
+			}
+
+			if (i === 1) {
+
+				container.style.backgroundColor = "#4c28824d";
+
+			}
+
+			if (i === 2) {
+
+				container.style.backgroundColor = "#A5967E4d";
+
+			}
+
+			if (i === 3) {
+
+				container.style.backgroundColor = "#77C06C4d";
+
+			}
+
+			if (i === 4) {
+
+				container.style.backgroundColor = "#3599CB4d";
+
+			}
+
+		});
+
+	});
+
+}
+
+//SLIDER TELEFONOS MOVILES//
+
+$(document).ready(function () {
+    function toggleSlider() {
+      var windowWidth = $(window).width();
+      var sliderMobile = $('.slider-mobile');
+  
+      if (windowWidth <= 767) {
+        sliderMobile.show();
+  
+        if (!sliderMobile.hasClass('slick-initialized')) {
+          // Configura el slider solo si no se ha inicializado
+          sliderMobile.slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            dots: true,
+            autoplay: true,
+            autoplaySpeed: 1500,
+            mobileFirst: true,
+            responsive: [
+              {
+                breakpoint: 768,
+                settings: {
+                  slidesToShow: 3,
+                  slidesToScroll: 1,
+                }
+              }
+            ]
+          });
+        }
+      } else {
+        sliderMobile.hide();
+        // Si el slider se ha inicializado, destrúyelo para evitar conflictos con la verificación de pantalla
+        if (sliderMobile.hasClass('slick-initialized')) {
+          sliderMobile.slick('unslick');
+        }
+      }
+    }
+  
+    // Llama a la función al cargar la página y al redimensionar la ventana
+    toggleSlider();
+    $(window).on('resize', toggleSlider);
+  });
+  
+
+
+// CARROUSEL OFERTAS
+
+document.addEventListener("DOMContentLoaded", function () {
+    const carousel = document.querySelector(".carousel");
+    const images = carousel.querySelectorAll("img");
+    const totalImages = images.length;
+    let currentIndex = 0;
+    let progressInterval;
+
+    function showImage(index) {
+        for (let i = 0; i < totalImages; i++) {
+            images[i].style.display = "none";
+        }
+        images[index].style.display = "block";
+        currentIndex = index;
+    }
+
+    function nextImage() {
+        currentIndex = (currentIndex + 1) % totalImages;
+        showImage(currentIndex);
+        resetProgress();
+    }
+
+    function prevImage() {
+        currentIndex = (currentIndex - 1 + totalImages) % totalImages;
+        showImage(currentIndex);
+        resetProgress();
+    }
+
+    const prevBtn = document.getElementById("prevBtn");
+    const nextBtn = document.getElementById("nextBtn");
+    const progressBar = document.getElementById("progress-bar");
+
+    prevBtn.addEventListener("click", function () {
+        prevImage();
+    });
+
+    nextBtn.addEventListener("click", function () {
+        nextImage();
+    });
+
+    function startProgress() {
+        progressBar.style.transition = "none";
+        progressBar.style.width = "0%";
+        progressBar.offsetWidth; // Truco para reiniciar la animación
+        progressBar.style.transition = "width 3s linear";
+        progressBar.style.width = "100%";
+    }
+
+    function resetProgress() {
+        clearInterval(progressInterval);
+        progressBar.style.width = "0%";
+        startProgress();
+    }
+
+    setInterval(nextImage, 5000);
+    showImage(currentIndex);
+    startProgress();
+});
+
+function nuevo() {
+    window.location.href='producto_nuevo.html'
+}
+
+function admin () {
+    pass = document.getElementById("pass").value;
+    if (pass !== null) {
+        // Tomar decisiones basadas en el valor ingresado
+        if (pass == "admin123") {
+          alert("Contraseña correcta");
+          window.location.href = 'admin.html';
+        } else {
+          alert("Contraseña incorrecta.");
+          window.location.href = 'index.html'
+        }
+      } else {
+        // El usuario presionó Cancelar en el prompt
+        alert("Operación cancelada, no ingresaste ninguna clave.");
+        window.location.href = 'index.html'
+      }
+    }
+
+  
+
+// validacion formulario //
+function validarFormulario() {
+    // Limpiar mensajes de error anteriores
+    document.getElementById('errorMessages').innerHTML = '';
+
+    // Obtener valores de los campos
+    nombre = document.getElementById('nombre').value
+    email = document.getElementById('email').value
+    asunto = document.getElementById('asunto').value
+    mensaje = document.getElementById('mensaje').value
+
+    // Validar nombre
+    if (nombre === '' || !/^[a-zA-ZáéíóúÁÉÍÓÚñÑüÜ\s]+$/.test(nombre) || /^\s/.test(nombre)) {
+        mostrarError('El nombre es obligatorio y debe contener solo letras.');
+        return;
+    }
+
+    // Validar email
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (email === '' || !emailRegex.test(email)) {
+        mostrarError('Ingrese un correo electrónico válido.');
+        return;
+    }
+
+    // Validar asunto (en este caso, no hay restricciones)
+    if (asunto === '') {
+        mostrarError('El asunto es obligatorio.');
+        return;
+    }
+
+    // Validar mensaje (en este caso, no hay restricciones)
+    if (mensaje === '') {
+        mostrarError('El mensaje es obligatorio.');
+        return;
+    }
+
+    // Si llegamos aquí, el formulario es válido. Puedes enviar los datos.
+   document.getElementById('nombre').value=""
+   document.getElementById('email').value=""
+   document.getElementById('asunto').value=""
+   document.getElementById('mensaje').value=""
+    alert('Formulario enviado con éxito.');
+    //
+    //
+
+}
+
+function mostrarError(message) {
+    const errorMessageContainer = document.getElementById('errorMessages');
+    const errorMessage = document.createElement('p');
+    errorMessage.textContent = message;
+    errorMessageContainer.appendChild(errorMessage);
+}
+
+
+
+
+// API REST
+
+document.addEventListener("DOMContentLoaded", async () => {
+    const apiUrl = 'https://fakestoreapi.com/products';
+
+    const productList = document.getElementById('product-list');
+
+    try {
+        const response = await fetch(apiUrl);
+        if (!response.ok) {
+            throw new Error('No se pudo obtener la información de la API');
+        }
+        const products = await response.json();
+
+        products.forEach(product => {
+            const productDiv = document.createElement('div');
+            productDiv.className = 'product';
+            productDiv.innerHTML = `
+                <img src="${product.image}" alt="${product.title}" class="product-image">
+                <h2 class="product-title">${product.title}</h2>
+                <p class="product-description">${product.description}</p>
+                <p class="product-price">Price: $${product.price}</p>
+            `;
+            productList.appendChild(productDiv);
+        });
+    } catch (error) {
+        console.error('Error al cargar productos:', error);
+    }
+});
+
+// logica mostrar mas//
+
+document.addEventListener('DOMContentLoaded', function () {
+    const showMoreButton = document.getElementById('showMoreButton');
+    const modal = document.getElementById('myModal');
+    const closeModal = document.getElementById('closeModal');
+
+    showMoreButton.addEventListener('click', function () {
+        modal.style.display = 'block';
+    });
+
+    closeModal.addEventListener('click', function () {
+        modal.style.display = 'none';
+    });
+
+    window.addEventListener('click', function (event) {
+        if (event.target === modal) {
+            modal.style.display = 'none';
+        }
+    });
+});
+
+
+
+
+
+     
+
+
